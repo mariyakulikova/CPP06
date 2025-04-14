566f8c0486c251b91cec1987b0a7c462eb2b77f1
mkulikov <mkulikov@student.42berlin.de>
2025-04-14 17:05:48 +0200

    refactor: remove pragma once

--- ex01/Data.hpp
+++ ex01/Data.hpp
@@ -6,11 +6,13 @@
 /*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 15:11:22 by mkulikov          #+#    #+#             */
-/*   Updated: 2025/04/14 15:26:00 by mkulikov         ###   ########.fr       */
+/*   Updated: 2025/04/14 17:05:30 by mkulikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
+#ifndef DATA_HPP
+#define DATA_HPP
+
 #include <iostream>
 
 struct Data
@@ -18,3 +20,5 @@ struct Data
 	int len;
 	std::string str;
 };
+
+#endif
